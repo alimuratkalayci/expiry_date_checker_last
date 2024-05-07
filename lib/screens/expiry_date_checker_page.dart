56123ebf902c89f04b4d0befe4d3ad8dabf5ec06
Ali Murat <alimuratkalayci@outlook.com>
2024-05-07 17:16:45 +0300

    home_page and epiry_date_checker_page added and main.dart updated.

---
+++ lib/screens/expiry_date_checker_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ExpiryDateChecker extends StatefulWidget {
+  const ExpiryDateChecker({super.key});
+
+  @override
+  State<ExpiryDateChecker> createState() => _ExpiryDateCheckerState();
+}
+
+class _ExpiryDateCheckerState extends State<ExpiryDateChecker> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text('deneme'),
+    );
+  }
+}
---
+++ lib/screens/home_page.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+import 'expiry_date_checker_page.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(child: const Text('Marketçi')),
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: ElevatedButton( onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => ExpiryDateChecker()),
+                    );
+
+                  },
+                      child: Text('Son Kullanım Tarihi Kontrol')),
+                ),
+              ],
+            ),
+          ),
+
+        ],
+      ),
+    );
+  }
+}
